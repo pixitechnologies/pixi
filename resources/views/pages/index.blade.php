ee49ce8cf3ac1e7215b08432d5a82f4b8abf8e54
Kassim <kasimuriyami@gmail.com>
2018-01-03 14:21:38 +0300

    index going thru

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,22 @@
+
+@extends('layouts.master')
+
+@section('content')
+
+    <div id="header" class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <h1>RESPONSIVE WEB DESIGN</h1>
+                    <P>Digital Solution For Your Business </P>
+                    <button class="btn btn-md btn-danger">Call us now</button>
+                    <button class="btn btn-md btn-danger">See our Projects</button>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <img src="{{asset('img/pixi-responsive-design.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
