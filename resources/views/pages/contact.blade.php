5b334ecba67075b480392b835f0c7924c6adcf3c
Reuben <im_23n@outlook.com>
2018-01-08 23:08:26 +0300

    Google Map added

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,7 +2,23 @@
 
 @section('content')
 
-    <h1>Contact</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1 style="font-family: 'B SIDE'">CONTACT US TODAY</h1>
+            </div>
+
+            <div class="col-md-6">
+                <iframe
+                        width="600"
+                        height="450"
+                        frameborder="0" style="border:0"
+                        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDnSgNbq3DIy_aV2eTB4yFTdqDhQtoqF34&q=record+stores+in+Seattle" allowfullscreen>
+                </iframe>
+            </div>
+
+        </div>
+    </div>
   
 
     @endsection
