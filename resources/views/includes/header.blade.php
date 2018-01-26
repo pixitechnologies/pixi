1f3828d6b7470b8340266748a0b8d7f7cbca93f0
Kassim <kasimuriyami@gmail.com>
2018-01-26 13:34:51 +0300

    changes done by kassim --Homepage

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -19,7 +19,7 @@
         <div class="collapse navbar-collapse" id="navbar">
 
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><i class="fa fa-map-marker"></i> Swahili St Dar es salaam</a></li>
+                <li><a href="https://www.google.com/maps/place/Pixi+Technologies/@-6.8207784,39.2757308,15z/data=!4m5!3m4!1s0x0:0xb6cba1f58c317054!8m2!3d-6.8207784!4d39.2757308"><i class="fa fa-map-marker"></i> Swahili St Dar es salaam</a></li>
                 <li><a href="#"><i class="fa fa-envelope"></i> info@pixitechnologies.com</a></li>
                 <li><a href="#"><i class="fa fa-mobile"></i> +255719907010</a></li>
                 <li><a href="/"><i class="fa fa-home"></i> HOME</a></li>
