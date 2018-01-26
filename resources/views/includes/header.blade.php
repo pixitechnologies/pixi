0e0cc867a482fc3d8bf17da90482e2a8a50556f1
Reuben <im_23n@outlook.com>
2018-01-26 12:54:23 +0300

    header changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -19,9 +19,12 @@
         <div class="collapse navbar-collapse" id="navbar">
 
             <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><i class="fa fa-map-marker"></i> Swahili St Dar es salaam</a></li>
+                <li><a href="#"><i class="fa fa-envelope"></i> info@pixitechnologies.com</a></li>
+                <li><a href="#"><i class="fa fa-mobile"></i> +255719907010</a></li>
                 <li><a href="/"><i class="fa fa-home"></i> HOME</a></li>
                 {{--<li><a href="/projects"><i class="fa fa-hand-o-right"></i> PROJECTS</a></li>--}}
-                {{--<li><a href="/about"><i class="fa fa-exclamation-circle"></i> ABOUT</a></li>--}}
+
                 <li><a href="#contact"><i class="fa fa-phone-square"></i> CONTACT</a></li>
 
             </ul>
